3e3d0311eedb82cce6a7539fd3aa6112693f6373
Changmini <cml039465@gmail.com>
2024-07-13 16:42:46 +0900

    .

--- src/main/java/ko/co/moodtracker/vo/DailyEntryVO.java
+++ src/main/java/ko/co/moodtracker/vo/DailyEntryVO.java
@@ -1,11 +1,18 @@
 package ko.co.moodtracker.vo;
 
 public class DailyEntryVO extends SearchVO {
+	private int seq;
 	private String title;
 	private String notes;
 	private String mood;
 	private String activities;
 	
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
 	public String getTitle() {
 		return title;
 	}
