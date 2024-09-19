6ca593fea53538939dc96e4c5db9bfdc88643714
Changmini <cml039465@gmail.com>
2024-09-19 20:52:26 +0900

    barchar 데이터 추가

--- src/main/java/kr/co/moodtracker/service/GraphService.java
+++ src/main/java/kr/co/moodtracker/service/GraphService.java
@@ -20,40 +20,53 @@ public class GraphService {
 
 	public Map<?,?> getMoodLevelData(int userId, DailySearchVO vo) {
 		vo.setUserId(userId);
-		Map<String, List<Object>> graphData = new HashMap<>();
+		Map<String, Map<?,?>> graphData = new HashMap<>();
+		Map<String, Object> lineChart = new HashMap<>();
+		Map<String, Object> barChart = new HashMap<>();
 		/* 라벨링 */
-		List<Object> labels = new ArrayList<Object>() {{
-			add("sunday"); add("monday"); add("tuesday");
-			add("wednesday"); add("thursday"); add("friday");
-			add("saturday");
-		}};
-		graphData.put("labels", labels);
+		lineChart.put("labels", DateHandler.dayOfWeeks);
+		barChart.put("labels", DateHandler.dayOfMonth);
 		
 		/* 데이터셋 */		
 		List<Map<String,Object>> metadata = graphMapper.getMoodLevelData(vo);
-		List<Object> datasets = new ArrayList<>();
-		Map<String, Object> dataset = null;
-		Integer[] data = null;
+		List<Object> lineDatasets = new ArrayList<>();
+		Map<String, Object> lineDataset = null;
+		int[] lineData = null;
+		List<Object> barDatasets = new ArrayList<>();
+		Map<String, Object> barDataset = null;
+		int[] barData = null;
 		String preLabel = "";
 		for (Map<String,Object> m : metadata) {
 			String label = (String) m.get("label");
 			/* 1~7 = 일요일~토요일 */
 			Integer weekNum = Integer.valueOf(m.get("weekNum").toString());
+			Integer dayOfMonth = Integer.valueOf(m.get("dayOfMonth").toString());
 			Integer level = Integer.valueOf(m.get("moodLevel").toString());
 			if (!label.equals(preLabel)) {
+				/* common */
 				preLabel = label;// 이전 라벨과 현재 라벨 비교
-				data = new Integer[7];
-				dataset = new HashMap<>();
-				dataset.put("label", label); // month (월)
-				dataset.put("data", data); // 요일별 mood_level 합산값
-				datasets.add(dataset);
+				/* Line chart */
+				lineData = new int[7];
+				lineDataset = new HashMap<>();
+				lineDataset.put("label", label); // month (월)
+				lineDataset.put("data", lineData); // 요일별 mood_level 합산값
+				lineDatasets.add(lineDataset);
+				/* Bar chart */
+				barData = new int[31];
+				barDataset = new HashMap<>();
+				barDataset.put("label", label); // month (월)
+				barDataset.put("data", barData);
+				barDatasets.add(barDataset);
 			}
-			/* mood_level 합산 */
-			Integer tmpLevel = data[weekNum-1];
-			tmpLevel = ((tmpLevel != null ? tmpLevel : 0) + level);
-			data[weekNum-1] = tmpLevel;
+			/* Line - mood_level 합산 */
+			lineData[weekNum-1] = lineData[weekNum-1] + level;
+			/* bar */
+			barData[dayOfMonth-1] = level;
 		}
-		graphData.put("datasets", datasets);
+		lineChart.put("datasets", lineDatasets);
+		barChart.put("datasets", barDatasets);
+		graphData.put("lineChart", lineChart);
+		graphData.put("barChart", barChart);
 		return graphData;
 	}
 	
