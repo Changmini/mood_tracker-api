f9919655ace02f47cb0b06127f190d899cc3b026
Changmini <cml039465@gmail.com>
2024-11-20 00:33:41 +0900

    mybatis : SqlSessionFactory 등록 방법 변경

--- src/main/java/kr/co/moodtracker/config/DbMoodtrackerConfig.java
+++ src/main/java/kr/co/moodtracker/config/DbMoodtrackerConfig.java
@@ -6,12 +6,12 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
 @MapperScan(basePackages = {"kr.co.moodtracker.mapper"})
@@ -29,9 +29,6 @@ public class DbMoodtrackerConfig {
 	@Value("${spring.datasource.password}")
 	private String password;
 	
-	@Autowired
-	ApplicationContext applicationContext; 
-	
 	@Bean
 	public DataSource dataSource() {
 		return DataSourceBuilder.create()
@@ -46,8 +43,10 @@ public class DbMoodtrackerConfig {
 	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
 		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
 		factoryBean.setDataSource(dataSource);
-		factoryBean.setConfigLocation(applicationContext.getResource("classpath:/mybatis/config/mybatis-config.xml"));
-		factoryBean.setMapperLocations(applicationContext.getResources("classpath:/mybatis/mapper/*Mapper.xml"));
+		factoryBean.setConfigLocation(new ClassPathResource("mybatis/config/mybatis-config.xml"));
+		factoryBean.setMapperLocations(
+				(new PathMatchingResourcePatternResolver()).getResources("classpath:/mybatis/mapper/*Mapper.xml")
+			);
 		return factoryBean.getObject();
 	}
 	
