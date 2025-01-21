29c677e9da26a53cb51cb07aed28991093741a93
Changmini <cml039465@gmail.com>
2025-01-21 19:42:25 +0900

    필요없는 import문 삭제

--- src/main/java/kr/co/moodtracker/config/SocketConfig.java
+++ src/main/java/kr/co/moodtracker/config/SocketConfig.java
@@ -2,12 +2,9 @@ package kr.co.moodtracker.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import kr.co.moodtracker.handler.ChatWebSocketHandler;
 
