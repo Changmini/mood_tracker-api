1cc216c467f61db6832e86918237470a3ece2b02
Changmini <cml039465@gmail.com>
2025-02-09 19:56:15 +0900

    hikari CP 적용하기

--- src/main/java/kr/co/moodtracker/config/DbMoodtrackerConfig.java
+++ src/main/java/kr/co/moodtracker/config/DbMoodtrackerConfig.java
@@ -6,38 +6,38 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @MapperScan(basePackages = {"kr.co.moodtracker.mapper"})
 public class DbMoodtrackerConfig {
-
-	@Value("${spring.datasource.url}")
-	private String url;
-	
-	@Value("${spring.datasource.driver-class-name}")
-	private String driverClassName;
-	
-	@Value("${spring.datasource.username}")
-	private String username;
-	
-	@Value("${spring.datasource.password}")
-	private String password;
 	
 	@Bean
-	public DataSource dataSource() {
-		return DataSourceBuilder.create()
-				.url(url)
-				.driverClassName(driverClassName)
-				.username(username)
-				.password(password)
-				.build();
+	@Primary
+	@ConfigurationProperties("spring.datasource")
+	public DataSourceProperties dataSourceProperties() {
+		return new DataSourceProperties();
 	}
+	
+	@Bean
+	@ConfigurationProperties("spring.datasource.configuration")
+	public HikariDataSource dataSource(DataSourceProperties properties) {
+		return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+	}
+	
+	 @Bean
+	 public DataSourceTransactionManager transactionManager(DataSource dataSource) {
+		 return new DataSourceTransactionManager(dataSource);
+	 }
 	
 	@Bean
 	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
