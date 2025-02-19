417d942e126945854afa19241bc5dcc69fc47bb2
Changmini <cml039465@gmail.com>
2025-02-19 23:57:42 +0900

    modify shell script

--- shell_auto_deploy.sh
+++ shell_auto_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-##### PROCESS KILL
+##### Process Kill -15 java
 processes=$(ps -ef | grep java | grep -v grep)
 if [ -z "$processes" ]; then
     echo "Java process is not running"
@@ -19,8 +19,42 @@ if ! command -v git &> /dev/null; then
 fi
 
 
+##########################################################
+########################## NODE ##########################
+##########################################################
+##### Git Check
+echo "frontend processing"
+cd
+cd mood_tracker-view
+if [ ! -d ".git" ]; then
+    echo "frontend-file does not have a .git repository"
+    exit 1
+fi
 
-##### GIT - API
+
+##### Git PULL
+echo "running 'git reset --hard'"
+git reset --hard
+echo "running 'git pull'"
+git pull
+
+
+##### Change URL-Origin info
+FILENAME="src/common.js"
+sed -i "s|^const API_HOST = \"localhost\"; *|//const API API_HOST = \"localhost\";|" "$FILENAME"
+sed -i "s|^//const API_HOST = \"3.38.99.65\"; *|const API API_HOST = \"3.38.99.65\";|" "$FILENAME"
+echo "API Origin modification completed"
+
+
+##### Apply 'npm run build'
+echo "npm run build...."
+npm run build
+
+
+##########################################################
+########################## JAVA ##########################
+##########################################################
+##### Git Check
 echo "backend processing"
 cd
 cd mood_tracker-api
@@ -28,30 +62,44 @@ if [ ! -d ".git" ]; then
     echo "backend-file does not have a .git repository"
     exit 1
 fi
+
+
+##### Apply 'gradlew clean'
+./gradlew clean
+
+
+##### Git PULL
 echo "running 'git reset --hard'"
 git reset --hard
 echo "running 'git pull'"
 git pull
 
+
+##### Set Database info
 FILENAME="src/main/resources/application.yml"
 USERNAME=
 PASSWORD=
-sed -i "0,/^username:/! { /^username:/ { s/^username: */username: $USERNAME/; t } }" "$FILENAME"
-sed -i "0,/^password:/! { /^password:/ { s/^password: */username: $PASSWORD/; t } }" "$FILENAME"
+sed -i "s/usrename:/usrename: $USERNAME/g" "$FILENAME"
+sed -i "s/password:/password: $PASSWORD/g" "$FILENAME"
 echo "DB-info modification completed"
 
+
+##### Apply 'chmod 777 gradlew'
 chmod 777 ./gradlew
 ./gradlew build
 
+
+##### Gradle process terminated
 processes=$(ps -ef | grep java | grep -v grep)
 echo "$processes" | while read -r line; do
         pid=$(echo "$line" | awk '{print $2}')
         echo "Java process PID $pid shutdown..."
         kill -15 "$pid"
+done
 
-cd build/lib
+##### Run jar
+cd build/libs
 nohup sudo java -jar mood_tracker-api-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
-
 
 
 echo "success"
